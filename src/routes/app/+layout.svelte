8e828e09b9747d2ebda4431f50bd2faa4980f8b3
Sam Walsh <samjwalsh05@gmail.com>
2024-06-29 15:09:20 +0100

    shit

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+      import * as Resizable from "$lib/components/ui/resizable";
+</script>
+
+<Resizable.PaneGroup direction="vertical" class="w-screen h-screen">
+    <Resizable.Pane>One</Resizable.Pane>
+    <Resizable.Handle />
+    <Resizable.Pane>Two</Resizable.Pane>
+  </Resizable.PaneGroup>
