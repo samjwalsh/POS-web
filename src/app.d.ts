5eb43a92c5157072acd491e070d0ce2a21a851e3
Sam Walsh <samjwalsh05@gmail.com>
2024-07-09 09:13:01 +0100

    auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,11 +3,17 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: import("lucia").User;
+			session: import("lucia").Session;
+		}
+
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
 }
 
-export {};
+export { };
+
+
--- src/routes/app/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad, Actions } from "./$types";
-
-export const load: PageServerLoad = async (event) => {
-    if (!event.locals.user) redirect(302, "/auth/login");
-    return {
-        username: event.locals.user.username
-    };
-};
