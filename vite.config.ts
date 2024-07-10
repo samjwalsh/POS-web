cb5b01ee87af74dc76ec538e6b2bbd285d2ac5ec
Sam Walsh <samjwalsh05@gmail.com>
2024-07-10 10:32:35 +0100

    fuck

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from 'vite';
 
 
 export default defineConfig({
-	plugins: [sveltekit()],
+    plugins: [sveltekit()],
+    build: {
+        rollupOptions: {
+            external: ['@node-rs/argon2']
+        },
+    },
+    optimizeDeps: {
+        exclude: ['@node-rs/argon2', '@node-rs/bcrypt']
+    },
 });
